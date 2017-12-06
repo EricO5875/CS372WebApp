48902c9e475e10a35d51c2e8c4ce1d06063e153c
Eric Olsen <olseeg01@students.ipfw.edu>
2017-12-06 08:38:54 +0000

    Updated queue to show only the user's media

--- views/queue.php
+++ views/queue.php
@@ -4,66 +4,67 @@ An queue page for entertainmentcenter.com
 
 -->
 
+<?php
+    //Connect to database
+    $connection = connect_to_db();
+
+    $movieResults = getUserMovieQueue($connection);
+
+    $bookResults = getUserBookQueue($connection);
+
+?>
+
         <div class="container">
             <h2>My Queue</h2>
-            
+
             <ul class="tabs">
         		<li class="tab-link current" data-tab="tab-1">Movies</li>
         		<li class="tab-link" data-tab="tab-2">TV Shows</li>
         		<li class="tab-link" data-tab="tab-3">Books</li>
         	</ul>
-            
+
                 <div id="tab-1" class="tab-content current">
                     <table class="table table-striped">
                         <tr>
                             <th>Cover</th>
                             <th>Title</th>
-                            <th>Genre</th>
                             <th>Status</th>
                             <th>Rating</th>
                             <th>Avg Rating</th>
                         </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/thelastjedi.jpg" alt="The Last Jedi cover"/></td>
-                            <td>Star Wars: The Last Jedi (2017)</td>
-                            <td>Action/Adventure/Fantasy</td>
-                            <td>Want to watch</td>
+
+                        <?php
+                            for($i = 0; $i < 5; $i++)
+                            {
+                                if ($result = $movieResults->fetch_assoc()) {
+                                    if ($result['Status'] = 0) {
+                                        $status = "Have Watched";
+                                    } else if ($result['Status'] = 1) {
+                                        $status = "Currently Watching";
+                                    } else {
+                                        $status = "Want to Watch";
+                                    }
+
+
+                                    echo "<tr>";
+                                    echo "<td><img class='rounded' src='" . $result['ImageURL'] . "' alt='" . $result['Title'] . "'/></td>";
+                                    echo "<td>" . $result['Title'] . "</td>";
+                                    echo "<td>" . $status . "</td>";
+                                    echo "<td style='width:300px'><div class='rateYo'></div></td>";
+                                    echo "<td>" . $result['AverageRating'] . "</td>";
+                                }
+                            }
+                        ?>
+                        <!--<tr>-->
+                        <!--    <td><img class="rounded" src="../images/thelastjedi.jpg" alt="The Last Jedi cover"/></td>-->
+                        <!--    <td>Star Wars: The Last Jedi (2017)</td>-->
+                        <!--    <td>Action/Adventure/Fantasy</td>-->
+                        <!--    <td>Want to watch</td>-->
                             <!--Width put in to keep stars from changing col size-->
-                            <td style = "width:300px"><div class="rateYo"></div></td>
-                            <td>N/A</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/it.jpg" alt="It (2017) cover"/></td>
-                            <td>It (2017)</td>
-                            <td>Drama/Horror/Thriller</td>
-                            <td>Want to watch</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>3.90</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/bladerunner2049.jpg" alt="Blade Runner 2049 cover"/></td>
-                            <td>Blade Runner 2049 (2017)</td>
-                            <td>Mystery/Sci-fi/Thriller</td>
-                            <td>Want to watch</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>4.25</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/blackpanther.jpg" alt="Black Panther cover"/></td>
-                            <td>Black Panther (2018)</td>
-                            <td>Action/Drama/Sci-fi</td>
-                            <td>Want to watch</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>N/A</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/whatwedointheshadows.jpg" alt="What We Do in the Shadows cover"/></td>
-                            <td>What We Do in the Shadows (2014)</td>
-                            <td>Comedy/Horror</td>
-                            <td>Want to watch</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>3.80</td>
-                        </tr>
+                        <!--    <td style = "width:300px"><div class="rateYo"></div></td>-->
+                        <!--    <td>N/A</td>-->
+                        <!--</tr>-->
+
                         <tr>
                             <td colspan="5"><a href="#">see more</a></td>
                         </tr>
@@ -87,7 +88,7 @@ An queue page for entertainmentcenter.com
                             <!--Width put in to keep stars from changing col size-->
                             <td style = "width:300px"><div class="rateYo"></div></td>
                             <td>4.45</td>
-        
+
                         </tr>
                         <tr>
                             <td><img class="rounded" src="../images/curbyourenthusiasm.jpg" alt="Curb Your Enthusiasm cover"/></td>
@@ -136,48 +137,40 @@ An queue page for entertainmentcenter.com
                             <th>Rating</th>
                             <th>Avg Rating</th>
                         </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/artofdeal.jpg" alt="Trump: Art of the Deal cover"/></td>
-                            <td>Trump: Art of the Deal</td>
-                            <td>Donald J. Trump, Tony Schwartz</td>
-                            <td>Want to read</td>
+
+                        <?php
+                            for($i = 0; $i < 4; $i++)
+                            {
+                                if ($result = $bookResults->fetch_assoc()) {
+
+                                    if ($result['Status'] = 0) {
+                                        $status = "Have Read";
+                                    } else if ($result['Status'] = 1) {
+                                        $status = "Currently Reading";
+                                    } else {
+                                        $status = "Want to Read";
+                                    }
+
+                                    echo "<tr>";
+                                    echo "<td><img class='rounded' src='" . $result['ImageURL'] . "' alt='" . $result['Title'] . "'/></td>";
+                                    echo "<td>" . $result['Title'] . "</td>";
+                                    echo "<td>" . $result['Author'] . "</td>";
+                                    echo "<td>" . $status . "</td>";
+                                    echo "<td style='width:300px'><div class='rateYo' data-rateYo-rating='" . $result['Rating'] . "'></div></td>";
+                                    echo "<td>" . $result['AverageRating'] . "</td>";
+                                }
+                            }
+                        ?>
+                        <!--<tr>-->
+                        <!--    <td><img class="rounded" src="../images/artofdeal.jpg" alt="Trump: Art of the Deal cover"/></td>-->
+                        <!--    <td>Trump: Art of the Deal</td>-->
+                        <!--    <td>Donald J. Trump, Tony Schwartz</td>-->
+                        <!--    <td>Want to read</td>-->
                             <!--Width put in to keep stars from changing col size-->
-                            <td style = "width:300px"><div class="rateYo"></div></td>
-                            <td>3.66</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/turtlesallthewaydown.jpg" alt="Turtles All the Way Down cover"/></td>
-                            <td>Turtles All the Way Down</td>
-                            <td>John Green</td>
-                            <td>Want to read</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>4.38</td>
-        
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/thebookthief.jpg" alt="The Book Thief cover"/></td>
-                            <td>The Book Thief</td>
-                            <td>Markus Zusak</td>
-                            <td>Want to read</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>4.36</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/themartian.jpg" alt="The Martian cover"/></td>
-                            <td>The Martian</td>
-                            <td>Andy Weir</td>
-                            <td>Want to read</td>
-                            <td> <div class="rateYo"></div></td>
-                            <td>4.39</td>
-                        </tr>
-                        <tr>
-                            <td><img class="rounded" src="../images/theprincessdiarist.jpg" alt="The Princess Diarist cover"/></td>
-                            <td>The Princess Diarist</td>
-                            <td>Carrie Fisher</td>
-                            <td>Want to read</td>
-                            <td><div class="rateYo"></div></td>
-                            <td>3.65</td>
-                        </tr>
+                        <!--    <td style = "width:300px"><div class="rateYo"></div></td>-->
+                        <!--    <td>3.66</td>-->
+                        <!--</tr>-->
+
                         <tr>
                             <td colspan="6"><a href="#">see more</a></td>
                         </tr>
